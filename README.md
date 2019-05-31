7e205a454276ae01adb5b22d3aa3efe0788433c7
Maël Nison <nison.mael@gmail.com>
2019-05-31 12:56:35 +0200

    Updates the readme

---
+++ README.md
@@ -0,0 +1,29 @@
+# PikaSprite
+
+A different interface for the [PokéSprite](https://github.com/msikma/pokesprite) icons.
+
+## Usage
+
+```
+$> yarn add pikasprite
+```
+
+```js
+import 'pikasprite/build/spritesheet.css';
+
+const Pikachu = () => <i className={`pkspr pokemon-pikachu`}/>;
+```
+
+## Icon List
+
+Consult the [gh-pages](https://arcanis.github.io/pikasprite) for the list of available icons.
+
+## License (MIT)
+
+> **Copyright © 2019 Mael Nison**
+>
+> Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+>
+> The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+>
+> THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ scripts/gen-spritesheet.sh
@@ -0,0 +1,123 @@
+#!/usr/bin/env bash
+
+set -e
+
+HERE_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
+BUILD_DIR="${HERE_DIR}/../build"
+TMP_DIR="$(mktemp -d)"
+
+cd "${HERE_DIR}/../icons"
+
+CSS_PATH="${BUILD_DIR}/spritesheet.css"
+IMAGE_PATH="${BUILD_DIR}/spritesheet-%%.png"
+JS_PATH="${BUILD_DIR}/spritesheet.js"
+LESS_PATH="${BUILD_DIR}/spritesheet.less"
+
+MONTAGE_FILE="${TMP_DIR}/montage.txt"
+
+COLUMNS=32
+
+rm -rf "${BUILD_DIR}"
+mkdir -p "${BUILD_DIR}"
+
+cat >> "${JS_PATH}" <<EOF
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.PikaSprite = factory();
+  }
+}(typeof self !== 'undefined' ? self : this, function () {
+  return [
+EOF
+
+cat >> "${LESS_PATH}" <<EOF
+.pkspr:before {
+  display: inline-block;
+  content: '';
+
+  width: 32px;
+  height: 32px;
+
+  background-image: url(./spritesheet-items.png);
+}
+.pkspr[class^="pokemon-"]:before, .pkspr[class*=" pokemon-"]:before {
+  width: 40px;
+  height: 30px;
+
+  background-image: url(./spritesheet-pkmns.png);
+}
+EOF
+
+for PASS in pkmns items; do
+    rm -f "${MONTAGE_FILE}"
+
+    echo "=== ${PASS} ==="
+    echo "Crawling..."
+
+    X=0
+    Y=0
+
+    case "${PASS}" in
+        pkmns) W=40 H=30 OPTS=(-name '*.png' -a -wholename './pokemon/*');;
+        items) W=32 H=32 OPTS=(-name '*.png' -a -not -wholename './pokemon/*');;
+    esac
+
+    find "${OPTS[@]}" | sed 's|^\./||' | while read ICON_FILE; do
+        RW=$(identify -format "%w" "$ICON_FILE")> /dev/null
+        RH=$(identify -format "%h" "$ICON_FILE")> /dev/null
+
+        if [[ $RW -ne $W || $RH -ne $H ]]; then
+            echo "Warning: ${ICON_FILE}: ${RW}x${RH} != ${W}x${H}"
+        fi
+
+        IFS=/ read -ra TAGS <<<"$(dirname "${ICON_FILE}")"
+        TAGS=("${TAGS[@]/regular}")
+
+        PRIMARY="${TAGS[0]}"
+        SECONDARIES=("${TAGS[@]:1}")
+        NAME=$(basename "${ICON_FILE}" .png)
+
+        CLASS_NAME="$(
+            printf '.pkspr.%s-%s' "${PRIMARY}" "${NAME}"
+            for TAG in "${SECONDARIES[@]}"; do
+                if [[ $TAG != "" ]]; then
+                    printf ".%s" "${TAG}"
+                fi
+            done
+        )"
+
+        printf '  "%s",\n' "${CLASS_NAME}" >> "${JS_PATH}"
+        printf '%s:before {background-position:%spx %spx}\n' "${CLASS_NAME}" "$(($X * -$W))" "$(($Y * -$H))" >> "${LESS_PATH}"
+
+        echo "${ICON_FILE}" >> "$MONTAGE_FILE"
+
+        X=$(($X + 1))
+        if [[ $X == $COLUMNS ]]; then
+            X=0
+            Y=$(($Y + 1))
+        fi
+    done
+
+    FINAL_IMAGE_PATH="${IMAGE_PATH//%%/$PASS}"
+
+    echo "Montage..."
+    montage -background transparent -tile "${X}x${Y}" -geometry "${W}x${H}" @"${MONTAGE_FILE}" "${FINAL_IMAGE_PATH}"
+
+    echo "Crushing..."
+    pngcrush -q -brute -ow "${FINAL_IMAGE_PATH}"
+done
+
+cat >> "${JS_PATH}" <<EOF
+  ];
+}));
+EOF
+
+yarn run -q lessc "${HERE_DIR}/../index.less" "${CSS_PATH}"
